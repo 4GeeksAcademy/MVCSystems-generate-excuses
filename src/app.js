81b86578f096964006c35dbcf269f096b7a8ad65
MVCSystems <carlosmerav@gmail.com>
2024-08-01 12:41:59 -0500

    Add code for generate excuses

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@
 // import "./assets/img/4geeks.ico";
 
 let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
+let action = ['eat', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
